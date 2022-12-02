e96af245e76ee9a3a4372899cae4e3fdc5c9e2f1
Zizheng Liu <2613043L@student.gla.ac.uk>
2022-12-02 19:09:08 +0000

    add board png

--- main.py
+++ main.py
@@ -4,13 +4,25 @@ from sys import exit
 pygame.init()
 
 def main():
-    window = pygame.display.set_mode(size=(500,500))
+    window = pygame.display.set_mode(size=(400,500))
+    pygame.display.set_caption("Sudoku")
+    clock = pygame.time.Clock()
+    font = pygame.font.Font(None,50)
+    board_surface = pygame.image.load('graphics/board.png')
+    note_surface = pygame.Surface((500,100))
+    note_surface.fill('White')
+    text_surface = font.render("My game", True, "BLACK")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+
+        window.blit(board_surface,(0,0))
+        window.blit(text_surface,(0,0))
+        window.blit(note_surface,(0,400))
         pygame.display.update()
+        clock.tick(60)
 
 if __name__=="__main__":
     # call the main function
