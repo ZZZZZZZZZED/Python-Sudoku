119a87102db771f7b0f5212ef164789221355392
Zizheng Liu <2613043L@student.gla.ac.uk>
2022-12-14 23:50:36 +0000

    try to update copied df in a loop

--- test.py
+++ test.py
@@ -10,34 +10,37 @@ df = pd.read_excel('resource/sudoku_base.xlsx')
 
 def make_empty(df, level:int):
     
+    copy_df = df
+
     # 9 x 9 game board
     full_slots = 9 * 9
+    slots = full_slots
 
     if level == 1:
         random_list = [0, 1] # when random choose 0, make this locate a empty number for user to fill.
-        mini_accept = 0.45 * full_slots
+        mini_accept = 0.5 * full_slots
     elif level == 3:
         random_list = [0, 0, 0, 1]
         mini_accept = 0.25 * full_slots
     else:
         random_list = [0, 0, 1]
-        mini_accept = 0.3 * full_slots
+        mini_accept = 0.35 * full_slots
     
     # traverse dataframe
     for i in range(9):
         for j in range(9):
             if random.choice(random_list) == 0:
-                df.at[j, i] = 0
-                slots = full_slots - 1
-                print(slots)
+                copy_df.at[j, i] = 0
+                slots = slots - 1
     if slots >= mini_accept:
-        print(slots)
-        print('accept')
-        print(df)
+        print('Accepted, {} slots available.'.format(slots))
+        return False
     else:
-        print('Not playable, {} only slots left, need {}.'.format(slots, mini_accept))
+        print('Not playable, only {} slots left, need {}.'.format(slots, mini_accept))
+        return True
             
 # def playable(df, pct):
-
-
-make_empty(df, 1)
+while make_empty(df, 2):
+    print(df)
+print(df)
+# make_empty(df, 2)
