daa5072bb1b0d361c7f7faab3b9bbaba9a0bd45c
Zizheng Liu <2613043L@student.gla.ac.uk>
2022-12-19 06:24:46 +0000

    real update

--- module.py
+++ module.py
@@ -8,6 +8,29 @@ given_list = []
 
 df = pd.read_excel('resource/sudoku_base.xlsx')
 
+def overwrite(pos, input):
+    name = 'given_' + str(pos[0]) + '_' + str(pos[1])
+    for g in given_list:
+        if g.name == name:
+            g.num = input
+
+def mousepos_to_boardpos(x, y):
+    min_x = []
+    min_y = []
+    for i in range(9):
+        name = 'given_' + str(i) + '_' + str(0)
+        for g in given_list:
+            if g.name == name:
+                temp = abs(g.window_x - x)
+                min_x.append(temp)
+    for j in range(9):
+        name = 'given_' + str(0) + '_' + str(j)
+        for g in given_list:
+            if g.name == name:
+                temp = abs(g.window_y - y)
+                min_y.append(temp)
+    print(min_x.index(min(min_x)), min_y.index(min(min_y)))
+    return (min_x.index(min(min_x)), min_y.index(min(min_y)))
 
 def make_empty(df, level:int):
     
